38ab71aba12779ad144295e020eaaa46a678a183
kimkhSotatek <kimkh@sotatek.com>
2025-02-16 13:30:51 +0900

    george: [FEAT] Changed Version
    - Tailwind CSS 버전 변경
    - style.css에 유틸리티 클래스 지시어 추가

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./style.css";
 import i18n from "./i18n";
-import "./assets/tailwind.css";
 
 const app = createApp(App);
 app.use(i18n);
